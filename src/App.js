0093bea7d70f79a4499afd3733eb8c58f601e63d
kasheefbasha <kasheefbasha1901@gmail.com>
2023-05-20 12:54:04 +0530

    second update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,142 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+
+
+/*
+
+Component will always return the jsx whereas the function will return 
+the value or variable
+
+below inside the App component it means reuse the component and dynamically change all
+the name at a time
+
+
+*/
+
+// function App() {
+//   const [state, setState] = useState("kalil")
+//   return (
+//     <div className="App">
+//       <Basic 
+//       name={state}
+//       />
+//       <Basic 
+//       name={state}
+//       />
+//       <Basic 
+//       name={state}
+//       />
+//       <Basic 
+//       name={state}
+//       />
+
+//       <button onClick={()=> setState("New Name from component")}>Change new Name</button>
+
+//     </div>
+//   );
+// }
+
+/*
+
+Life cycle of the components : 
+1. Initialization (states and props)
+2. Mounting (Inside the components the operations which is going to happen it will
+             mount the some operations)
+3. updation - it will update the props and state after mounting 
+4. unmounting - components that go away 
+
+
+useEffect : 
+- useEffect is a react hook
+syntax :
+useEffect(()=>{}, []) -> this is a function with the dependency array
+
+
+
+example : 
+
+function Basic({name}){
+  
+  const [show, setShow] = useState(true);
+
+  //Mounting - if the dependency array is empty it will work or execute only one time
+  // but its not empty it will continuously
+  useEffect(()=>{
+    console.log("I'm Mounted", show)
+  }, [show])
+
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <button onClick={() => setShow(!show)}>Change Name</button>
+    </div>
+  )
+}
+
+******** Most important *****************
+Need to understand the useState and useEffect - read carefully
+....................................................
+
+statefull component : 
+
+component were it contains the state
+
+function App() {
+  const [state, setState] = useState("kalil")
+  return (
+    <div className="App">
+      <Basic 
+      name={state}
+      />
+    </div>
+  );
+}
+
+export default App;
+
+stateless component :
+
+components were doesnot contains the state, even props can be present
+
+function Basic({name}){
+  
+  return (
+    <div>
+      <h1>{name}</h1>
+      <button onClick={() => setShow(!show)}>Change Name</button>
+    </div>
+  )
+}
+..............................................
+
+*/
+
+
+// export default App;
+
+// function Basic({name}){
+  
+//   const [show, setShow] = useState(true);
+
+//   //Mounting - if the dependency array is empty it will work or execute only one time
+//   // but its not empty it will continuously
+//   useEffect(()=>{
+//     console.log("I'm Mounted", show)
+//   }, [show])
+
+
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <button onClick={() => setShow(!show)}>Change Name</button>
+//     </div>
+//   )
+// }
+
+
+
 /*
 Props drilling : you are passing the same data from the components to child
 it will give you the similar data from GreatGrandFather to child
@@ -90,4 +226,6 @@ function Child({state, setState}){
     </div>
   )
 }
+
+
 
