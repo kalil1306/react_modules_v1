b7b3d0e4941106c4da22a75d0dc0ae178c9fc4c1
kasheefbasha <kasheefbasha1901@gmail.com>
2023-05-20 12:37:06 +0530

    second push

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 import { useState } from 'react';
 
 /*
-Props drilling
+Props drilling : you are passing the same data from the components to child
+it will give you the similar data from GreatGrandFather to child
 
+
+Example : 
+In this below example we are not getting a data from GreatgrandFather to child,
+instead of we are drilling down from GreatGrandFather > GrandFater > Father > Child
+
+you need to pass the data from each cases or drilling towards the end,need to 
+pass all the tree cycle to get and pass the data till the end of the tree
+
+-----------------------------------------
+state : 
+first param uses the variable
+second params uses the change of the variable 
 
 */
 
@@ -14,6 +27,7 @@ function App() {
     <div className="App">
       <GreatGrandFather
       state={state}
+      setState = {setState}
       />
     </div>
   );
@@ -22,49 +36,53 @@ function App() {
 export default App;
 
 
-function GreatGrandFather({state}){
+function GreatGrandFather({state, setState}){
   return (
     <div>
       GreatGrandFather
       <h1>{state}</h1>
       <GrandFather
       state={state}
+      setState = {setState}
       />
     </div>
   )
 }
 
-function GrandFather({state}){
+function GrandFather({state, setState}){
   return (
     <div>
       GrandFather
       <h1>{state}</h1>
       <Father
       state={state}
+      setState = {setState}
       />
     </div>
   )
 }
 
 
-function Father({state}){
+function Father({state, setState}){
   return (
     <div>
       Father
       <h1>{state}</h1>
       <Child
       state={state}
+      setState = {setState}
       />
     </div>
   )
 }
 
 
-function Child({state}){
+function Child({state, setState}){
   return (
     <div>
       Child
       <h1>{state}</h1>
+      <button onClick={() => setState("Kasheef")}>Change State</button>
     </div>
   )
 }
