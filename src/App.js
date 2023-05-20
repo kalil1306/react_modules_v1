2b824108c3f8bf6ff9823d413ce68585d4528a82
kasheefbasha <kasheefbasha1901@gmail.com>
2023-05-20 12:26:47 +0530

    first-update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+/*
+Props drilling
+
+
+*/
 
 function App() {
+  const [state, setState] = useState("Kalil")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GreatGrandFather
+      state={state}
+      />
     </div>
   );
 }
 
 export default App;
+
+
+function GreatGrandFather({state}){
+  return (
+    <div>
+      GreatGrandFather
+      <h1>{state}</h1>
+      <GrandFather
+      state={state}
+      />
+    </div>
+  )
+}
+
+function GrandFather({state}){
+  return (
+    <div>
+      GrandFather
+      <h1>{state}</h1>
+      <Father
+      state={state}
+      />
+    </div>
+  )
+}
+
+
+function Father({state}){
+  return (
+    <div>
+      Father
+      <h1>{state}</h1>
+      <Child
+      state={state}
+      />
+    </div>
+  )
+}
+
+
+function Child({state}){
+  return (
+    <div>
+      Child
+      <h1>{state}</h1>
+    </div>
+  )
+}
+
